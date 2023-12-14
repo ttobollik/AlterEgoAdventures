19cf99ea75cd6df30fc4763416a3784cf8b5750a
Gabriel Rocha <rochagbriel@gmail.com>
2023-12-14 17:20:10 +0100

    Update app.routes.ts
    - Created Route to 'faq'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
+import { HeroComponent } from './hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'faq',
+    component: FaqComponent,
+  },
+  {
+    path: '',
+    component: HeroComponent,
+  },
+];
