6bdca9553ad8235ed2a588b47c89e94590b72949
Gabriel Rocha <rochagbriel@gmail.com>
2023-12-14 17:19:41 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { FaqComponent } from './faq/faq.component';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    HeroComponent,
+    NgbAccordionModule,
+    FaqComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
