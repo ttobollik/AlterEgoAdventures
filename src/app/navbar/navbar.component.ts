e436da4ea048c86192457161b0e04a7845892bbd
Gabriel Rocha <rochagbriel@gmail.com>
2024-01-03 18:54:26 +0100

    Dropdown menu Upcoming
    - Linked to Airtable content

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { UpcomingService } from '../services/upcoming.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgbDropdownModule],
+  providers: [UpcomingService],
+  imports: [NgbDropdownModule, CommonModule, HttpClientModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+
+export class NavbarComponent implements OnInit {
   isCollapsed = false;
+  upcomingData: any = {};
+
+  constructor(private upcomingService: UpcomingService) {}
+
+  ngOnInit(): void {
+    this.upcomingService.getUpcomingData().subscribe((data) => {
+      this.upcomingData = data;
+      console.log(this.upcomingData);
+    });
+  }
 }
