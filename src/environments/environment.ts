244f5d71877b9b61349146c3efde9afcb112215c
theresa <theresa.tobollik@gmail.com>
2024-03-09 18:10:25 +0100

    add env

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: '',
+  apiKey:
+    '',
+  apiFaq: 'tblY92layIjDPMl1c',
+  apiUpcoming: 'tbloVGXSfcaoJYI8l',
+  apiGallery: 'tblicHhy7Ehn9sLgw',
+  apiUpcomingPictures: 'tblACHTYQhrz5aoX6',
+  apiTestimonials: 'tbl4n3XdpqU7AaX9I',
+  apiNews: 'tblklAt9XOw63cuVp',
+  apiKeyBrevo: '',
+};
