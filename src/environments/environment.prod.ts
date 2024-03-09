38d5cb1c84321dee939d7423661f940a2dede2dd
theresa <theresa.tobollik@gmail.com>
2024-03-09 15:51:44 +0100

    prod api keys

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiUrl: 'AIRTABLE_URL',
+  apiKey: 'AIRTABLE_API_KEY',
+  apiFaq: 'tblY92layIjDPMl1c',
+  apiUpcoming: 'tbloVGXSfcaoJYI8l',
+  apiGallery: 'tblicHhy7Ehn9sLgw',
+  apiUpcomingPictures: 'tblACHTYQhrz5aoX6',
+  apiTestimonials: 'tbl4n3XdpqU7AaX9I',
+  apiNews: 'tblklAt9XOw63cuVp',
+  brevoApiKey: 'BREVO_API_KEY',
+};
