12427ea6d50fc30fb8df546825baa39a9d2cf48e
theresa <theresa.tobollik@gmail.com>
2024-03-09 17:33:26 +0100

    rename keys

--- dist/alter-ego-adventures/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-Q6XMMR7L.mjs";import"./chunk-UZ4IONPM.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'AIRTABLE_URL',
-  apiKey: 'AIRTABLE_API_KEY',
+  apiUrl: 'AIRTABLE_URL_PLACEHOLDER',
+  apiKey: 'AIRTABLE_API_KEY_PLACEHOLDER',
   apiFaq: 'tblY92layIjDPMl1c',
   apiUpcoming: 'tbloVGXSfcaoJYI8l',
   apiGallery: 'tblicHhy7Ehn9sLgw',
   apiUpcomingPictures: 'tblACHTYQhrz5aoX6',
   apiTestimonials: 'tbl4n3XdpqU7AaX9I',
   apiNews: 'tblklAt9XOw63cuVp',
-  apiKeyBrevo: 'BREVO_API_KEY',
+  apiKeyBrevo: 'BREVO_API_KEY_PLACEHOLDER',
 };
